b568c85c6db47f30f076e436b5cf9e2d65022ea3
caby-erin <caby27@gmail.com>
2023-09-16 13:13:36 -0500

    can favorite other users locations yay

--- api/locationData.js
+++ api/locationData.js
@@ -106,6 +106,109 @@ const communityLocations = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const getUserFavorites = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/userFavorites/${uid}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        const favoriteKeys = Object.keys(data);
+        const userFavorites = favoriteKeys.map((key) => ({
+          favoriteFirebaseKey: key,
+          locationFirebaseKey: data[key].locationFirebaseKey,
+          uid: data[key].uid,
+        }));
+        resolve(userFavorites);
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const addFavorite = (locationFirebaseKey, uid) => new Promise((resolve, reject) => {
+  const favorite = {
+    locationFirebaseKey,
+    uid,
+    favorites: true,
+  };
+  fetch(`${endpoint}/userFavorites/${uid}.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(favorite),
+  })
+    .then((response) => response.json())
+    .then(({ name }) => {
+      const patchedFavorite = { ...favorite, firebaseKey: name };
+      resolve(patchedFavorite);
+    })
+    .catch(reject);
+});
+
+const deleteFavorite = (locationFirebaseKey, uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/userFavorites/${uid}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const favoriteKey = Object.keys(data).find(
+        (key) => data[key].locationFirebaseKey === locationFirebaseKey,
+      );
+      if (favoriteKey) {
+        fetch(`${endpoint}/userFavorites/${uid}/${favoriteKey}.json`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((deleteResponse) => {
+            if (deleteResponse.ok) {
+              resolve();
+            } else {
+              reject(new Error('Failed to remove favorite location'));
+            }
+          })
+          .catch(reject);
+      } else {
+        reject(new Error('Favorite not found'));
+      }
+    })
+    .catch(reject);
+});
+
+const getCommunityFavorites = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/userFavorites.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getEveryLocation = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/locations.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
 export {
   getLocations,
   deleteLocation,
@@ -115,4 +218,9 @@ export {
   updateLocation,
   favoriteLocations,
   communityLocations,
+  getUserFavorites,
+  addFavorite,
+  deleteFavorite,
+  getCommunityFavorites,
+  getEveryLocation,
 };
